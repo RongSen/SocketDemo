1c39099507405048dc688e75938163e1d47341d4
John <John>
2017-05-15 09:30:41 +0800

    Initial commit

---
+++ src/com/wrox/ClusterNodeServlet.java
@@ -0,0 +1,14 @@
+package com.wrox;
+
+/**
+ * Created by Administrator on 2017-05-12.
+ */
+@ClientEndpoint
+public class ClusterNodeServlet {
+
+    private Session session;
+    private String nodeId;
+
+    public void init(){
+    }
+}
